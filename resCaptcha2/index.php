15e90f0ac9813148f89eef2f40d6eaf434d773de
Joel R M Reis <69910076+jrmreis@users.noreply.github.com>
2020-09-04 11:02:39 -0300

    whats e link voltar ok

--- resCaptcha2/index.php
+++ resCaptcha2/index.php
@@ -12,13 +12,33 @@
         <div>Formulário Teste</div>
         
         <form id="form1" name="form1" action="controllers/ControllerForm.php" method="post">
-            <input type="text" id="nome" name="nome"><br>
+            <input type="text" id="nome" name="nome"><br> 
             <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response"><br>
             <input type="submit"><br>
         </form>
         <script src="https://www.google.com/recaptcha/api.js?render=<?php echo SITE_KEY; ?>"></script>
         <script src="libraries/Javascript.js"></script>
-      
+        
+       
+        
+        
+        
+      <style>
+.whatsapp {
+    position: fixed;
+    top: 82%;
+    left: 1%;
+    padding: 10px;
+    z-index: 10000000;
+}
+</style>
+<div>
+    <a href="https://api.whatsapp.com/send?phone=5511997992651" 
+       target="_blank">
+       <img  class="whatsapp" src="https://images.tcdn.com.br/static_inst/integracao/imagens/whatsapp.png" />
+    </a>
+</div>
+
     </body>
 </html>
 
